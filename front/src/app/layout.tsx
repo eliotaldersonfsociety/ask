c0aa35c06af956a0c7f0cb74f1a63a11c72d08c5
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-02-16 15:19:24 -0500

    Update layout.tsx

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";  // Cambié a Inter como ejemplo
 import "./globals.css";
 import { CartProvider } from "./pages/context/CartContext";
 import { SessionProvider } from "./pages/context/SessionContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",  // Definir la variable para usarla
   subsets: ["latin"],
 });
 
@@ -26,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${inter.variable} antialiased`}>
         <SessionProvider>
           <CartProvider>
             {children}
